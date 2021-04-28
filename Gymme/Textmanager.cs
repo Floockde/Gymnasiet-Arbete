088aca739fae1142205173d3bcb23d53758b7af0
Floockde <54839781+Floockde@users.noreply.github.com>
2021-04-28 15:18:16 +0200

    Initial commit

--- Gymme/Textmanager.cs
+++ Gymme/Textmanager.cs
@@ -8,7 +8,11 @@ namespace Gymme
         public Textmanager()
         {
             XmlDocument story = new XmlDocument();
-            story.Load("C:\\Users\\Administrator\\OneDrive - Rättviks Kommun\\source\\Gymme\\Gymnasiet-Arbete\\Story");
+            story.Load("C:\\Users\\Administrator\\OneDrive - Rättviks Kommun\\source\\Gymme\\Gymnasiet-Arbete\\Story\\Story.xml");
+
+            XmlNode node = story.DocumentElement.SelectSingleNode("p");
+
+            string dialogue = node.InnerText;
         }
 
         private static Textmanager instance = null;
